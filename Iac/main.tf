a2b3501ffdf8308eff3aa239f277470b47f0fef2
ShunilGupta <guptashunil@gmail.com>
2023-05-07 16:53:43 +0530

    Update main.tf

--- Iac/main.tf
+++ Iac/main.tf
@@ -1,16 +1,16 @@
 module "RG" {
-    source                =  "/Data/RG"
+    source                =  "/modules/ExistingResources"
     rg_name                  = var.rg_name
 }
 
 module "STORAGEACCOUNT" {
-    source                 = "/Data/STORAGE_ACCOUNT"
+    source                 = "/modules/ExistingResources"
     storage_account_name   = var.storage_account_name
     rg_name                = module.RG.name
 }
 
 module "FUNCTIONAPP" {
-    source                  = "/MODULES/Functionapp"
+    source                  = "/MODULES/NewResources"
     function_app            = var.function_app
     app_service_plan        = var.app_service_plan
     rg_name                 = module.RG.name
